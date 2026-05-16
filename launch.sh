6437d0e7ae9a40fd33196dd1a8d0f50979e20977
drw <arif.pens@gmail.com>
2026-05-16 11:08:31 +0700

    change emoji symbol to svg

---
+++ launch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+KIOSK_URL="index.html"
+
+unclutter -idle 0.1 -grab -root &
+
+xrandr --auto
+chromium \
+  --noerrdialogs \
+  --no-memcheck \
+  --no-first-run \
+  --start-maximized \
+  --disable \
+  --disable-translate \
+  --disable-infobars \
+  --disable-suggestions-service \
+  --disable-save-password-bubble \
+  --disable-session-crashed-bubble \
+  --incognito \
+  --kiosk $KIOSK_URL
+
